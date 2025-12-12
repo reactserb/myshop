30029a4ff4eda5c1547ede969cd69edd94ddf01e
Robert Gizzatullin <gizzatullin.robert@mail.ru>
2025-12-12 23:50:48 +0300

    some fixes for deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,18 @@ const montserrat = Montserrat({
 	subsets: ['latin', 'cyrillic'],
 })
 
-const baseUrl =
-	process.env.NEXT_PUBLIC_BASE_URL ||
-	process.env.VERCEL_URL ||
-	'http://localhost:3000'
+const getBaseUrl = () => {
+  const url = process.env.NEXT_PUBLIC_BASE_URL ?? 
+    (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000')
+  
+  try {
+    return new URL(url).toString()  // ← Валидная строка
+  } catch {
+    return 'http://localhost:3000'
+  }
+}
+
+const baseUrl = getBaseUrl()
 
 export const metadata: Metadata = {
 	metadataBase: new URL(baseUrl),
